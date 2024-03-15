f7d2b55009190707873adfd3a2c046ec00404cc3
Kapkov Efim <kapkov.efim@triasoft.com>
2024-03-15 17:00:08 +0300

    Создание detached подписи по данным.

--- MyLab.SmevClient/Crypt/Interop/Advapi32/NativeApi.cs
+++ MyLab.SmevClient/Crypt/Interop/Advapi32/NativeApi.cs
@@ -69,7 +69,7 @@ namespace MyLab.SmevClient.Crypt
             return InteropLinux.CryptHashData(hHash, pbData, dataLen, flags);
         }
 
-        public static  bool CryptGetHashParam(
+        public static bool CryptGetHashParam(
             [In] HashSafeHandle hHash,
             [In] uint dwParam,
             [In, Out] IntPtr pbData,
@@ -96,7 +96,7 @@ namespace MyLab.SmevClient.Crypt
             return InteropLinux.CryptSetHashParam(hHash, dwParam, pbData, dwFlags);
         }
 
-        public static  bool CryptSignHash(
+        public static bool CryptSignHash(
             [In] HashSafeHandle hHash,
             [In] uint keySpec,
             [In] IntPtr description,
@@ -109,6 +109,101 @@ namespace MyLab.SmevClient.Crypt
                 return InteropWindows.CryptSignHash(hHash, keySpec, description, flags, signature, ref signatureLen);
             }
             return InteropLinux.CryptSignHash(hHash, keySpec, description, flags, signature, ref signatureLen);
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pSignPara"></param>
+        /// <param name="fDetachedSignature">
+        ///    Значение TRUE , если это отсоединяемая сигнатура.
+        ///     В противном случае — FALSE.Если для этого параметра задано значение TRUE, в pbSignedBlob кодируется только хэш со знаком.
+        ///     В противном случае кодируются как rgpbToBeSigned, так и хэш со знаком.
+        ///    </param>
+        /// <param name="rgpbToBeSigned">Массив на данные которые подписываем.</param>
+        /// <param name="signature">Подпись</param>
+        /// <returns></returns>
+        internal unsafe static bool CryptSignMessage(
+                       [In] ref CRYPT_SIGN_MESSAGE_PARA pSignPara,
+                       [In, MarshalAs(UnmanagedType.Bool)] bool fDetachedSignature,
+                       [In] byte[] rgpbToBeSigned,
+                       [Out] out byte[] signature
+                       )
+        {
+            unsafe
+            {
+                // размер данных для подписи
+                uint* sizesData = stackalloc uint[] { (uint)rgpbToBeSigned.LongLength };
+
+                // фиксация данных для подписи
+                fixed (byte* ptrData = &rgpbToBeSigned[0])
+                {
+                    // массив с исходными данными
+                    IntPtr* rgpbToBeSignedPtr = stackalloc IntPtr[] { new IntPtr(ptrData) };
+
+                    uint signatureLength = 0;
+                   
+                    // резервирование данных
+                    if (!CryptSignMessage(ref pSignPara, fDetachedSignature, 1, new IntPtr(rgpbToBeSignedPtr), new IntPtr(sizesData), IntPtr.Zero, ref signatureLength))
+                    {
+                        signature = null;
+                        return false;
+                    }
+
+                    var signatureBuffer = Marshal.AllocHGlobal((int)signatureLength);
+                    try
+                    {
+                        // создание подписи
+                        if (!CryptSignMessage(ref pSignPara, fDetachedSignature, 1, new IntPtr(rgpbToBeSignedPtr), new IntPtr(sizesData), signatureBuffer, ref signatureLength))
+                        {
+                            signature = null;
+                            return false;
+                        }
+                        signature = new byte[signatureLength];
+                        Marshal.Copy(signatureBuffer, signature, 0, (int)signatureLength);
+                    }
+                    finally
+                    {
+                        Marshal.FreeHGlobal(signatureBuffer);
+                    }
+
+
+                }
+            }
+
+            return true;
+        }
+
+        private unsafe static bool CryptSignMessage(
+                [In] ref CRYPT_SIGN_MESSAGE_PARA pSignPara,
+                // Значение TRUE , если это отсоединяемая сигнатура.
+                // В противном случае — FALSE. Если для этого параметра задано значение TRUE, в pbSignedBlob кодируется только хэш со знаком.
+                // В противном случае кодируются как rgpbToBeSigned , так и хэш со знаком.
+                [In, MarshalAs(UnmanagedType.Bool)] bool fDetachedSignature,
+                //Количество элементов массива в rgpbToBeSigned и rgcbToBeSigned.
+                //Этот параметр должен иметь значение один, если только fDetachedSignature не имеет значение TRUE.
+                [In] uint cToBeSigned,
+                // Массив указателей на буферы, содержащие содержимое для подписи.
+                [In] IntPtr rgpbToBeSigned,
+
+                // Массив размеров (в байтах) буферов содержимого, на которые указывает rgpbToBeSigned.
+                [In] IntPtr rgcbToBeSigned,
+
+                // Указатель на буфер для получения закодированного хэша со знаком, если fDetachedSignature имеет значение TRUE,
+                // или на закодированное содержимое и хэш со знаком, если fDetachedSignature имеет значение FALSE
+                [Out] IntPtr signature,
+
+                // Указатель на DWORD , указывающий размер буфера pbSignedBlob (в байтах).
+                // При возврате функции эта переменная содержит размер подписанного и закодированного сообщения в байтах
+                [In, Out] ref uint signatureLength)
+        {
+            if (IsWindows)
+            {
+
+                return InteropWindows.CryptSignMessage(ref pSignPara, fDetachedSignature, cToBeSigned, rgpbToBeSigned, rgcbToBeSigned, signature, ref signatureLength);
+            }
+            return InteropLinux.CryptSignMessage(ref pSignPara, fDetachedSignature, cToBeSigned, rgpbToBeSigned, rgcbToBeSigned, signature, ref signatureLength);
         }
     }
 }
